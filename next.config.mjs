0e4040436bd4772ff2497e61a2f14279fc9ac8e4
Matthew Pandolfo <matt@pandolfo.com>
2024-06-19 19:40:49 -0400

    add env

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
     TOKEN_SECRET: process.env.TOKEN_SECRET,
     MONGODB_URI: process.env.MONGODB_URI,
     GITHUB_OAUTH_TOKEN: process.env.GITHUB_OAUTH_TOKEN,
+    NEXT_AWS_ACCESS_KEY_ID: process.env.NEXT_AWS_ACCESS_KEY_ID,
+    NEXT_AWS_SECRET_ACCESS_KEY: process.env.NEXT_AWS_SECRET_ACCESS_KEY,
   },
 };
 
