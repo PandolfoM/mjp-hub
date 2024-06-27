63f04ac1e7d5a0b9b1ce67216b44d9d518115806
Matthew Pandolfo <matt@pandolfo.com>
2024-06-26 20:33:21 -0400

    update verify page

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -5,6 +5,21 @@ import React, { useState } from "react";
 import Button from "../components/button";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  password: z.string().min(1, { message: "Required" }),
+  confirmPassword: z.string().min(1, { message: "Required" }),
+});
 
 function Verify() {
   const router = useRouter();
@@ -12,40 +27,70 @@ function Verify() {
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    if (password !== confirmPassword) return setError("Passwords do not match");
-    if (password === "") return setError("Password missing");
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+    if (data.password !== data.confirmPassword)
+      return setError("Passwords do not match");
+    if (data.password === "") return setError("Password missing");
 
     try {
-      await axios.post("/api/users/createpassword", { password });
+      await axios.post("/api/users/createpassword", {
+        password: data.password,
+      });
       router.push("/");
     } catch (error: any) {
       setError(error.response.data.error);
     }
   };
   return (
-    <div className="flex w-full h-full flex-col justify-center">
-      <div className="bg-gradient-radial to-80% from-card/20 to-card/5 rounded-sm p-2 h-72 flex flex-col gap-5">
-        <h3 className="text-center text-lg">Create Password</h3>
-        <form className="h-full flex flex-col" onSubmit={handleSubmit}>
-          <div className="flex flex-col h-full gap-2">
-            <Input
-              placeholder="Password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+    <div className="flex flex-col gap-3 items-center justify-center h-full w-full text-center">
+      <div className="flex flex-col w-[90%] sm:w-1/4">
+        <h1 className="font-bold text-md mb-[20px]">Create Password</h1>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="flex flex-col gap-[5px] text-left">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            <Input
-              placeholder="Confirm Password"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Confirm Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-          {error && <p className="text-sm text-error pb-2">{error}</p>}
-          <Button type="submit">Submit</Button>
-        </form>
+            {error && <p className="text-sm text-error pb-2">{error}</p>}
+            <Button type="submit" className="mt-[20px]">
+              Submit
+            </Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
